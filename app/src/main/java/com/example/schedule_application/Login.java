5f46e37a1ccb7c0707a792c0fb14aec840e1e030
nicolaskawer <nicolaskawer@gmail.com>
2024-05-30 19:08:03 +0300

    var1

--- app/src/main/java/com/example/schedule_application/Login.java
+++ app/src/main/java/com/example/schedule_application/Login.java
@@ -2,18 +2,23 @@ package com.example.schedule_application;
 
 import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
@@ -25,7 +30,7 @@ public class Login extends AppCompatActivity {
     Button buttonLogin;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
-    TextView textview;
+    TextView textview, forgotTextLink;
 
     @Override
     public void onStart() {
@@ -48,7 +53,10 @@ public class Login extends AppCompatActivity {
         buttonLogin = findViewById(R.id.btn_login);
         progressBar=findViewById(R.id.progressBar);
         textview = findViewById(R.id.registerNow);
+        forgotTextLink = findViewById(R.id.ForgotPassword);
+        //open the page
         textview.setOnClickListener(new View.OnClickListener() {
+            // this function says "if u click on the button that navigate to the registration page then call the registration class"
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Registration.class);// navigate to the registration page
@@ -56,16 +64,54 @@ public class Login extends AppCompatActivity {
                 finish();
             }
         });
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText resetMail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password ?");
+                passwordResetDialog.setMessage("Enter Your Email To Received Reset Link");
+                passwordResetDialog.setView(resetMail);
+                //now we will have 2 option if the user clicked yes or no
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //extract the email and send reset link
+                        String mail = resetMail.getText().toString();
+                        mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void Avoid) {
+                                Toast.makeText(Login.this, "\"Resat Link Send To Your Email",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this, "Error ! Reset Link is Not Send" + e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
+                    }
+                });
+                //display the other dialog
+                passwordResetDialog.create().show();
+
+            }
+        });
+        //this function work when the user click on the button of "log in"
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
                 String email, password;
+
                 email = String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
-
-
                 if(TextUtils.isEmpty(email)) {
                     Toast.makeText(Login.this, "Enter email", Toast.LENGTH_SHORT).show();
                     return;
@@ -74,11 +120,17 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(Login.this, "Enter password", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                if(password.length()<6){
+                    editTextPassword.setError("Password must be 6 characters or more");
+                }
+                // after the user clicked then the progressbar will be visible
+                progressBar.setVisibility(View.VISIBLE);
+                //this function has taken from the firebase web its for the data
                 mAuth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
+                                // if task successful means if the user has successfully logged in
                                 if (task.isSuccessful()) {
                                     Toast.makeText(getApplicationContext(), "Login Successfully", Toast.LENGTH_SHORT).show();
                                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
@@ -87,10 +139,11 @@ public class Login extends AppCompatActivity {
                                 } else {
                                     Toast.makeText(Login.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
-
+                                    progressBar.setVisibility(View.GONE);
                                 }
                             }
                         });
+
 
 
             }
--- app/src/main/java/com/example/schedule_application/Registration.java
+++ app/src/main/java/com/example/schedule_application/Registration.java
@@ -14,19 +14,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registration extends AppCompatActivity {
-    TextInputEditText editTextEmail, editTextPassword;
+    public static final String TAG = "TAG";
+    TextInputEditText editTextEmail, editTextPassword, editTextName, editTextLastName, editTextPhone;
     Button buttonReg;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
     TextView textview;
+    FirebaseFirestore fstore;
+    String userID;
 
     @Override
     public void onStart() {
@@ -46,9 +55,14 @@ public class Registration extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
+        editTextName = findViewById(R.id.name);
+        editTextLastName = findViewById(R.id.lastname);
+        editTextPhone = findViewById(R.id.phone);
         buttonReg = findViewById(R.id.btn_register);
         progressBar=findViewById(R.id.progressBar);
         textview = findViewById(R.id.loginNow);
+        fstore = FirebaseFirestore.getInstance();
+        // this function says "if u click on the button that navigate to the login page then call the login class"
         textview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,15 +72,26 @@ public class Registration extends AppCompatActivity {
             }
         });
 
+        if(mAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);// if the user already loged in
+            startActivity(intent);
+            finish();
+        }
 
 
+
+
+        //this function work when the user click on the button of "register now"
         buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
-                String email, password;
-                email = String.valueOf(editTextEmail.getText());
-                password = String.valueOf(editTextPassword.getText());
+                String email, password, name, last_name, phone;
+                email = editTextEmail.getText().toString().trim();
+                password = editTextPassword.getText().toString();
+                name = editTextName.getText().toString();
+                last_name = editTextLastName.getText().toString();
+                phone = String.valueOf(editTextPhone.getText());
 
 
                 if(TextUtils.isEmpty(email)) {
@@ -77,22 +102,59 @@ public class Registration extends AppCompatActivity {
                     Toast.makeText(Registration.this, "Enter password", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                if(password.length()<6){
+                    editTextPassword.setError("Password must be 6 characters or more");
+                }
 
+                if(TextUtils.isEmpty(name)) {
+                    Toast.makeText(Registration.this, "Enter name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(last_name)) {
+                    Toast.makeText(Registration.this, "Enter last name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(phone)) {
+                    Toast.makeText(Registration.this, "Enter phone number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // after the user clicked then the progressbar will be visible
+                progressBar.setVisibility(View.VISIBLE);
 
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
+                                // if task successful means if the user has successfully created
                                 if (task.isSuccessful()) {
                                     Toast.makeText(Registration.this, "Account has successfully created",
                                             Toast.LENGTH_SHORT).show();
+                                    userID = mAuth.getCurrentUser().getUid();
+                                    //creating collection called user and inside that we will have the other things
+                                    DocumentReference documentReference = fstore.collection("users").document(userID);
+                                    //creating Hase map  he made it as <string,object>
+                                    Map<String, String> user = new HashMap<>();
+                                    //and here now we are inserting our data as key and object
+                                    user.put("First name", name);
+                                    user.put("Last name", last_name);
+                                    user.put("Phone", phone);
+                                    user.put("Email", email);
+                                    documentReference.set(user).addOnSuccessListener((OnSuccessListener)  (aVoid)-> {
+                                                Log.d(TAG,"onSuccesses: user profile is created for "+ userID);
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.d(TAG, "onFailure: " + e.toString());
+                                        }
+                                    });
+                                    //in this line he wrote "startActivity(new Intent(getApplicationContext(),MainActivity.class));" but i didn't and still os working
 
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Toast.makeText(Registration.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
-
+                                    progressBar.setVisibility(View.GONE);
                                 }
                             }
                         });
